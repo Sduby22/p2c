703e90f1faac91236abd77eb3ff34a28502ebf6b
sduby <supersduby@gmail.com>
2022-03-04 22:25:50 +0800

    dep: add logging library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[]) {
   printf("HelloWorld\n");
+  for (int i = 0; i != 3; i++) {
+    spdlog::info("test info log {}", i);
+    spdlog::warn("test warn log {}", i);
+    spdlog::error("test error log {}", i);
+  }
   return 0;
 }
