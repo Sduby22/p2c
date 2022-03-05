2f119b54fa93b256b4238f6d1749e67660aba68a
sduby <supersduby@gmail.com>
2022-03-05 15:11:34 +0800

    add unit test framework Catch2

---
+++ tests/test_example.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch.hpp>
+#include <cmath>
+#include <string>
+
+TEST_CASE("Negative can't be sqrted.") {
+  auto fail = std::sqrt(-1);
+  REQUIRE(std::isnan(fail));
+}
+
+TEST_CASE("Should return helloworld.") {
+  REQUIRE([]() -> std::string {
+    return "helloworld";
+  }() == "helloworld");
+}
