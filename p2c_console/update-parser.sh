d7f90cb8a7d0c580873c5a7a9c12aac03b792b80
osennyaya <supersduby@gmail.com>
2022-04-23 23:33:25 +0800

    fix: add dependencies

--- p2c_console/update-parser.sh
+++ p2c_console/update-parser.sh
@@ -3,5 +3,6 @@ parser_root="../"
 cd $parser_root
 echo 'building p2c module'
 bash buildall.sh
+mkdir -p $workdir'/packages/renderer/src/wasm/'
 cp bin/libparser.* $workdir'/packages/renderer/src/wasm/'
 echo 'output copied.'
