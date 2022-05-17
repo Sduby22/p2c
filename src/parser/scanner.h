d7d2ba999552c2dcdf344fc2ff072355f8bce235
osennyaya <supersduby@gmail.com>
2022-05-17 15:38:48 +0800

    fix: gui err loc

--- src/parser/scanner.h
+++ src/parser/scanner.h
@@ -7,6 +7,7 @@
  ***********************************/
 
 /* 重要 */
+#include "location.hh"
 #if !defined(yyFlexLexerOnce)
 #undef yyFlexLexer
 #define yyFlexLexer p2c_FlexLexer // 根据prefix修改
@@ -20,13 +21,18 @@
 
 #include "parser.hpp"
 
+extern p2c::location loc;//声明位置实例
+
 namespace p2c {
+
 
 class Scanner : public yyFlexLexer {
 private:
 
 public:
-  Scanner() {}
+  Scanner() {
+    loc = p2c::location();
+  }
 
   virtual p2c::Parser::symbol_type
   nextToken(); // 不需要手动实现这个函数，Flex会生成YY_DECL宏定义的代码来实现这个函数
